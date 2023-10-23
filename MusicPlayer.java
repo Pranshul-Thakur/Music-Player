ca898aeae61d563be112e202b244ecf9f1d51b13
Pranshul-Thakur <118863617+Pranshul-Thakur@users.noreply.github.com>
2023-10-23 13:17:20 +0530

    Update MusicPlayer.java

--- MusicPlayer.java
+++ MusicPlayer.java
@@ -1,270 +1,202 @@
+import java.util.LinkedList;
+
 import java.util.Scanner;
+import java.util.Random;
 
-class Node {
-    int data;
-    Node next;
+class Song {
+    // This is the Song class, we use it to store song information.
+    private String title;
+    private String artist;
 
-    Node(int data) {
-        this.data = data;
-        this.next = null;
+    public Song(String title, String artist) {
+        // This is the constructor for creating a new song.
+        this.title = title;
+        this.artist = artist;
+    }
+
+    public String getTitle() {
+        // This method returns the title of the song.
+        return title;
+    }
+
+    public String getArtist() {
+        // This method returns the artist of the song.
+        return artist;
+    }
+
+    @Override
+    public String toString() {
+        // This method provides a nice formatted string for the song.
+        return title + " by " + artist;
     }
 }
 
-public class MusicPlayer {
-    private static int c = 0;
-    private static Node head = null;
-    private static int b = 0;
-    private static int pos = 0;
-    private static int p = 0;
-    private static int l;
-    private static int inf;
-    private static String str;
-    private static String[] songs = {
-            "Blinding_lights.wav", "Too_many_nights.wav", "Art_deco.wav",
-            "Night_changes.wav", "Money_Trees.wav", "Perfect.wav",
-            "Popular.wav", "Ice_cream.wav", "After_dark.wav",
-            "Calm_down.wav", "Sdp_Interlude.wav"
-    };
+class MusicPlayer {
+    // This is our MusicPlayer class that handles playing and managing songs.
+    private LinkedList<Song> playlist = new LinkedList<>();
+    private int currentSongIndex = 0;
+    private boolean isShuffleMode = false;
+    private boolean isRepeatOneSong = false;
 
+    public void addSong(Song song) {
+        // You can use this method to add a song to the playlist.
+        playlist.add(song);
+    }
+
+    public void removeSong(int index) {
+        // This method allows you to remove a song from the playlist.
+        if (index >= 0 && index < playlist.size()) {
+            playlist.remove(index);
+            if (index <= currentSongIndex) {
+                currentSongIndex--;
+            }
+            System.out.println("Song removed from the playlist.");
+        } else {
+            System.out.println("Invalid song index. Please try again.");
+        }
+    }
+
+    public void toggleShuffleMode() {
+        // This toggles the shuffle mode on and off for random song selection.
+        isShuffleMode = !isShuffleMode;
+        if (isShuffleMode) {
+            System.out.println("Shuffle mode is enabled.");
+        } else {
+            System.out.println("Shuffle mode is disabled.");
+        }
+    }
+
+    public void toggleRepeatOneSong() {
+        // You can use this to toggle repeat mode for playing one song repeatedly.
+        isRepeatOneSong = !isRepeatOneSong;
+        if (isRepeatOneSong) {
+            System.out.println("Repeat one song mode is enabled.");
+        } else {
+            System.out.println("Repeat one song mode is disabled.");
+        }
+    }
+
+    public void play() {
+        // When you call this, it plays the current song in the playlist.
+        if (playlist.isEmpty()) {
+            System.out.println("Playlist is empty. Add songs first.");
+        } else {
+            Song currentSong = playlist.get(currentSongIndex);
+            System.out.println("Now playing: " + currentSong);
+        }
+    }
+
+    public void next() {
+        // This method lets you play the next song in the playlist.
+        if (playlist.isEmpty()) {
+            System.out.println("Playlist is empty. Add songs first.");
+        } else {
+            if (isShuffleMode) {
+                currentSongIndex = new Random().nextInt(playlist.size());
+            } else {
+                currentSongIndex = (currentSongIndex + 1) % playlist.size();
+            }
+            play();
+        }
+    }
+
+    public void previous() {
+        // Use this to go back to the previous song in the playlist.
+        if (playlist.isEmpty()) {
+            System.out.println("Playlist is empty. Add songs first.");
+        } else {
+            if (isShuffleMode) {
+                currentSongIndex = new Random().nextInt(playlist.size());
+            } else {
+                currentSongIndex = (currentSongIndex - 1 + playlist.size()) % playlist.size();
+            }
+            play();
+        }
+    }
+
+    public void displayPlaylist() {
+        // This displays the current playlist with song numbers.
+        System.out.println("Playlist:");
+        int songNumber = 1;
+        for (Song song : playlist) {
+            System.out.println(songNumber + ". " + song);
+            songNumber++;
+        }
+    }
+}
+
+public class Main {
+    // Here's our main program where the user interacts with the player.
     public static void main(String[] args) {
+        MusicPlayer player = new MusicPlayer();
         Scanner scanner = new Scanner(System.in);
-        int choice;
 
         while (true) {
-            mainMenu();
-            choice = scanner.nextInt();
+            System.out.println("\nMenu:");
+            System.out.println("1. Add a song");
+            System.out.println("2. Remove a song");
+            System.out.println("3. Toggle shuffle mode");
+            System.out.println("4. Toggle repeat one song");
+            System.out.println("5. Play current song");
+            System.out.println("6. Play next song");
+            System.out.println("7. Play previous song");
+            System.out.println("8. Display playlist");
+            System.out.println("9. Quit");
+
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume the newline character
+
             switch (choice) {
                 case 1:
-                    list();
-                    System.out.println("Press '0' to return to the main menu.");
-                    scanner.nextInt();
+                    // Adding songs to the playlist
+                    System.out.print("Enter the title of the song: ");
+                    String title = scanner.nextLine();
+                    System.out.print("Enter the artist of the song: ");
+                    String artist = scanner.nextLine();
+                    Song song = new Song(title, artist);
+                    player.addSong(song);
+                    System.out.println("Song added to the playlist.");
                     break;
                 case 2:
-                    list();
-                    System.out.println("Press '0' to stop adding.");
-                    while (createPlaylist()) {
-                    }
+                    // Removing a song from the playlist
+                    System.out.print("Enter the index of the song to remove: ");
+                    int indexToRemove = scanner.nextInt();
+                    player.removeSong(indexToRemove - 1); // Adjust for 0-based index
                     break;
                 case 3:
-                    displayPlaylist();
-                    System.out.println("Press '0' to return to the main menu.");
-                    scanner.nextInt();
+                    // Toggling shuffle mode on or off
+                    player.toggleShuffleMode();
                     break;
                 case 4:
-                    displayPlaylist();
-                    playSearch();
+                    // Toggling repeat mode for playing one song repeatedly
+                    player.toggleRepeatOneSong();
                     break;
                 case 5:
-                    System.out.println("The playlist currently is:");
-                    displayPlaylist();
-                    System.out.println("List of songs available is:");
-                    list();
-                    createPlaylist();
+                    // Playing the current song
+                    player.play();
                     break;
                 case 6:
-                    prev();
+                    // Moving on to the next song
+                    player.next();
                     break;
                 case 7:
-                    next();
+                    // Going back to the previous song
+                    player.previous();
                     break;
                 case 8:
-                    displayPlaylist();
-                    deleteSong();
+                    // Displaying the current playlist
+                    player.displayPlaylist();
                     break;
                 case 9:
-                    System.out.println("Thank you for using our music player.\nPlease use us again....");
+                    // Saying goodbye and exiting the program
+                    System.out.println("Goodbye!");
+                    scanner.close();
                     System.exit(0);
                 default:
-                    System.out.println("Please select a valid option.");
+                    // Handling an invalid choice
+                    System.out.println("Invalid choice. Please try again.");
             }
-        }
-    }
-
-    private static void mainMenu() {
-        System.out.println("----------------------------------------------------------------------");
-        System.out.println("                              MAIN MENU");
-        System.out.println("----------------------------------------------------------------------");
-        System.out.println("   1   |  Display all available songs");
-        System.out.println("   2   |  Create a playlist");
-        System.out.println("   3   |  Show playlist");
-        System.out.println("   4   |  Play the music from the playlist");
-        System.out.println("   5   |  Add a song to the playlist");
-        System.out.println("   6   |  Play previous track");
-        System.out.println("   7   |  Play next track");
-        System.out.println("   8   |  Delete a song from the playlist");
-        System.out.println("   9   |  Exit music player");
-        System.out.println("----------------------------------------------------------------------");
-        System.out.println("                        Enter your choice below");
-    }
-
-    private static void list() {
-        System.out.println("----------------------------------------------------------------------");
-        for (int i = 0; i < songs.length; i++) {
-            System.out.println((i + 1) + "." + songs[i]);
-        }
-        System.out.println("12. Back To Main Menu.");
-        System.out.println("----------------------------------------------------------------------");
-    }
-
-    private static boolean createPlaylist() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter The Song Index Number and Its Position: ");
-        int n = scanner.nextInt();
-        if (n == 0) {
-            return false;
-        }
-
-        int p = scanner.nextInt();
-        if (p <= 0 || p > c + 1) {
-            System.out.println("Enter a valid position.");
-            return true;
-        } else {
-            c++;
-            Node newNode = new Node(n);
-            if (p == 1) {
-                newNode.next = head;
-                head = newNode;
-            } else {
-                Node current = head;
-                for (int i = 0; i < p - 2; i++) {
-                    current = current.next;
-                }
-                newNode.next = current.next;
-                current.next = newNode;
-            }
-            return true;
-        }
-    }
-
-    private static void displayPlaylist() {
-        int i = 1;
-        Node current = head;
-        if (current == null) {
-            System.out.println("Playlist is empty.");
-        } else {
-            while (current != null) {
-                int songIndex = current.data;
-                String songName = assign(songIndex);
-                System.out.println("----------------------------------------------------------------------");
-                System.out.println("   " + i + "   |  " + songName);
-                System.out.println("----------------------------------------------------------------------");
-                current = current.next;
-                i++;
-            }
-        }
-    }
-
-    private static String assign(int b) {
-        return songs[b - 1];
-    }
-
-    private static void song(String str) {
-        System.out.println("The song currently playing is " + str);
-        // Play the song here using Java's audio library or an external tool.
-    }
-
-    private static void playSearch() {
-        Scanner scanner = new Scanner(System.in);
-        int p = 0;
-        Node current = head;
-        System.out.print("Enter the Index of the song you want to play: ");
-        pos = scanner.nextInt();
-        while (current != null) {
-            p++;
-            if (p == pos) {
-                b = 1;
-                break;
-            }
-            current = current.next;
-        }
-        if (b == 1) {
-            str = assign(current.data);
-            song(str);
-            inf = current.data;
-        }
-        l = p;
-    }
-
-    private static void prev() {
-        int n;
-        Node current = head;
-        if (l == 0) {
-            System.out.println("No song was played previously.");
-        } else {
-            int k = 0;
-            while (current != null) {
-                k++;
-                if (l - 1 == k) {
-                    n = current.data;
-                    str = assign(n);
-                    song(str);
-                    break;
-                }
-                current = current.next;
-            }
-            p--;
-        }
-    }
-
-    private static void next() {
-        Node current = head;
-        int k = 0;
-        int size = 0;
-        while (current != null) {
-            size++;
-            current = current.next;
-        }
-        if (l == size) {
-            System.out.println("This is the last song of the playlist.");
-        } else {
-            current = head;
-            while (current != null) {
-                k++;
-                if (l + 1 == k) {
-                    int n = current.data;
-                    str = assign(n);
-                    song(str);
-                    break;
-                }
-                current = current.next;
-                p = p + 1;
-            }
-        }
-    }
-
-    private static void deleteSong() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the Index of the song you want to delete: ");
-        int p = scanner.nextInt();
-        if (p <= 0 || p > c) {
-            System.out.println("Enter a valid position.");
-        } else {
-            c--;
-            if (p == 1) {
-                head = head.next;
-            } else {
-                Node current = head;
-                for (int i = 0; i < p - 2; i++) {
-                    current = current.next;
-                }
-                Node temp = current.next;
-                current.next = temp.next;
-                temp = null;
-            }
-        }
-    }
-
-    private static void info() {
-        switch (inf) {
-            case 1:
-                System.out.println("Information for song 1...");
-                break;
-            case 2:
-                System.out.println("Information for song 2...");
-                break;
-            // Add more cases for other songs.
-            default:
-                System.out.println("No song is being played currently, please play a song to get more info");
         }
     }
 }
